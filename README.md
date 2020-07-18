d40da75a260c61d858b650e79020d1c1202aa50a
Eeshaan Achar <eeshaan77@gmail.com>
2020-07-18 17:47:16 +0530

    organised files

---
+++ README.md
@@ -0,0 +1,6 @@
+# Flipkart Tracker
+
+### Instructions
+Create a CSV file with the name 'products.csv' having 2 columns (without headers)
+1. Link to product, and
+2. Wanted price respectively
--- crawler.py
+++ crawler.py
@@ -3,28 +3,20 @@ try:
     import webbrowser
     from bs4 import BeautifulSoup
 
-    fh = open("./flipkart_links_and_price.csv")
-    dictionary = dict()
-    for line in fh.readlines():
-        if line != '\n':
-            url, wanted_price = line.split(',')
-            dictionary[url] = int(wanted_price)
-    fh.close()
-
     discounted_items = list()
-    for url, wanted_price in dictionary.items():
-        page = requests.get(url)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        product = soup.find(class_="_35KyD6").text
-        price = soup.find(class_="_1vC4OE _3qQ9m1").text.replace(',','')[1:]
-        print(product + "\n₹" + price + "\n")
-        if int(price) <= wanted_price:
-            discounted_items.append(url)
+    with open("products.csv") as fh:
+        for line in fh.readlines():
+            url, wanted_price = line.split(',')
+            page = requests.get(url)
+            soup = BeautifulSoup(page.content, 'html.parser')
+            product = soup.find(class_="_35KyD6").text
+            price = soup.find(class_="_1vC4OE _3qQ9m1").text.replace(',','')[1:]
+            print(product + "\n₹" + price + "\n")
+            if int(price) <= int(wanted_price):
+                discounted_items.append(url)
 
     for url in discounted_items:
         webbrowser.open(url)
-
-    input("Done")
         
-except:
-    input("Some error occurred!")
+except Exception as message:
+    input(message)
